f92bbc9d7d28a5e8fdb49a2c3b2173830ed8c29a
Suresh V <126890641+Sureshv24@users.noreply.github.com>
2026-04-17 17:03:14 +0530

    Java Script

---
+++ task6.js
@@ -0,0 +1,4 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+console.log("Full Array:", fruits)
+console.log("First Fruit:", fruits[0])
+console.log("Last Fruit:", fruits[fruits.length - 1])
---
+++ task7.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "Naveen",
+    age: 20,
+    course: "MERN"
+};
+
+console.log("Name:", student.name)
+console.log("Age:", student.age)
