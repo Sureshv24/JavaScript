cfb6030deb7ee45a0f6e1cb3dfbf22938da51bb2
Suresh V <126890641+Sureshv24@users.noreply.github.com>
2026-04-22 11:18:32 +0530

    data

---
+++ task13.js
@@ -0,0 +1,12 @@
+let students = [
+ {id:1, name:"Naveen", mark:85, course:"MERN"},
+ {id:2, name:"John", mark:45, course:"Python"},
+ {id:3, name:"Priya", mark:72, course:"Java"},
+ {id:4, name:"Arun", mark:95, course:"React"}
+];
+
+
+for(let i=0; i<2; i++){
+    console.log(students[i]);
+}
+console.log("10");
